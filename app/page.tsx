f8d1edd70723170d9dbc1fe033601b8dc5f1edc2
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-03 19:42:50 +0300

    feat : verify if tailwind works

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="relative">
-    <div>
+   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
+    mx-auto sm:px-10 px-5
+   ">
+    <div className="max-w-7xl w-full">
       <h1>
         Hello
       </h1>
