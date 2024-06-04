2b58837bc5ed0e566b4a0d22002e3dcea16a75f2
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-04 19:03:47 +0300

    feat : TextGerateEffect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
     return (
@@ -21,7 +22,8 @@ const Hero = () => {
             <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.04]
             bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
                 {/* Radial gradient for the container to give a faded look */}
-                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+                bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             </div>
 
             <div className='flex justify-center relative my-20'>
@@ -31,6 +33,10 @@ const Hero = () => {
                      text-center text-blue-100 max-w-80'>
                         Dynamic Web Magic with Next.js
                     </h2>
+                    <TextGenerateEffect
+                        className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                        words='Transforming Conceps into Seamless Experiences'
+                    />
                 </div>
             </div>
         </div>
