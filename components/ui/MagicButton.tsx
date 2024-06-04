0126757b1fd949291fec992eede290fec8eb4d1e
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-04 19:22:24 +0300

    feat : react icons

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -10,10 +10,14 @@ type MagicButtonProps = {
 
 const MagicButton = ({ title, icon, position, handleClick, otherClasses }: MagicButtonProps) => {
     return (
-        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <button className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px]">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            <span className={`inline-flex h-full w-full cursor-pointer items-center 
+            justify-center rounded-lg bg-slate-950 px-7 py-1 text-sm font-medium
+             text-white backdrop-blur-3xl ${otherClasses}`}>
+                {position === "left" && icon}
                 {title}
+                {position === "right" && icon}
             </span>
         </button>
     )
