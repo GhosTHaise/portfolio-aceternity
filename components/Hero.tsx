5ae0d81b90ba91c9cc805d403ae1a899ecf997f3
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-04 19:28:53 +0300

    faet : change banner text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,7 +37,7 @@ const Hero = () => {
                     </h2>
                     <TextGenerateEffect
                         className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                        words='Transforming Conceps into Seamless Experiences'
+                        words='Transforming Conceps into Seamless User Experiences'
                     />
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                         Hi, I&apos;am Fitiavana, a FullStack Developer based in Madagascar.
