bd4fb9e746bb4db4baed9ecf952e446347d8c9aa
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-06 21:41:29 +0300

    faet : image container

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import Image from "next/image";
 
 export const BentoGrid = ({
     className,
@@ -52,6 +53,20 @@ export const BentoGridItem = ({
                     "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
             }}
         >
+            <div className={`${id === 6 && "flex justify-center h-full"}`}>
+                <div className="w-full h-full absolute">
+                    {
+                        img && (
+                            <img
+                                src={img}
+                                alt={img}
+                                className={cn(imgClassName, 'object-cover object-center')}
+                            />
+                        )
+                    }
+                </div>
+            </div>
+
             <div className="group-hover/bento:translate-x-2 transition duration-200">
                 <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
                     {title}
