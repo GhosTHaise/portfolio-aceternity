9e71b48b9f5ea949390319606c1393b479a470b0
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-06 21:44:02 +0300

    faet : image container

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -65,6 +65,17 @@ export const BentoGridItem = ({
                         )
                     }
                 </div>
+                <div className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"}`}>
+                    {
+                        spareImg && (
+                            <img
+                                src={spareImg}
+                                alt={spareImg}
+                                className={`object-cover object-center w-full h-full`}
+                            />
+                        )
+                    }
+                </div>
             </div>
 
             <div className="group-hover/bento:translate-x-2 transition duration-200">
