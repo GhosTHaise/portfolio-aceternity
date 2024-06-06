7ee1647bc17a160e395c091991c80eb45f80da68
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-06 21:36:28 +0300

    faet : gradient from gradient.io

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -46,6 +46,11 @@ export const BentoGridItem = ({
                 "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
                 className
             )}
+            style={{
+                background: "rgb(4,7,29)",
+                backgroundColor:
+                    "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+            }}
         >
             <div className="group-hover/bento:translate-x-2 transition duration-200">
                 <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
