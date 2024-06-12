a7df33749d97a8f886215e5089904fa7d1bd115c
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-12 22:23:26 +0300

    feat : show link as title

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -10,15 +10,15 @@ const RecentProject = () => {
                 A small selection of {' '}
                 <span className='text-purple'>recent projects</span>
             </h1>
-            <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
+            <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
                 {
                     projects.map(({ id, title, des, img, iconLists, link }) => (
-                        <div key={id} className='lg:min-h-[32.5rem] h-[25rem] 
-                        flex items-center justify-center sm:w-96 w-[80ch] '>
-                            <PinContainer title={title} href={link}>
-                                <div className='relative flex items-center justify-center sm:w-96 w-[80vw]
-                                overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
-                                    <div className='relative w-ful h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                        <div key={id} className='sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] 
+                            flex items-center justify-center sm:w-[570px]  w-[80vh] '>
+                            <PinContainer title={link} href={link}>
+                                <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw]
+                                overflow-hidden sm:h-[40vh] lg:h-[30vh] mb-10'>
+                                    <div className='relative w-ful h-full overflow-hidden lg:rounded-3xl rounded-xl bg-[#13162d]'>
                                         <img src='/bg.png' alt="bg-img" />
                                     </div>
                                     <img src={img} alt={title} className='z-10 absolute bottom-0' />
