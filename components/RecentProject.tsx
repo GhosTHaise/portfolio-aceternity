63d38cebd1af0361c4f76af1a7b95dbdda46d674
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-12 20:57:49 +0300

    feat : list icons

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -23,6 +23,30 @@ const RecentProject = () => {
                                     </div>
                                     <img src={img} alt={title} className='z-10 absolute bottom-0' />
                                 </div>
+                                <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                                    {title}
+                                </h1>
+
+                                <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                                    {des}
+                                </p>
+                                <div className='flex items-center justify-between mt-6 mb-3'>
+                                    <div className='flex items-center'>
+                                        {
+                                            iconLists.map((icon, index) => (
+                                                <div
+                                                    key={icon}
+                                                    className='border border-white/[0.2] rounded-full bg-black
+                                                    lg:w-10 lg:h-10 w-8 h-8'
+                                                    style={{ transform: `translateX(-${5 * index * 2}px)` }}
+
+                                                >
+                                                    <img src={icon} alt={icon} className='p-2' />
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                </div>
                             </PinContainer>
                         </div>
                     ))
