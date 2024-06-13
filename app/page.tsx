bedc357d3c3b458a3a57ff0babd7e705e30af1d8
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-13 20:54:15 +0300

    feat : modify floating nav

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
     mx-auto sm:px-10 px-5
    ">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{ name: 'Home', link: "/", icon: <FaHome /> }]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProject />
