4a705d8a20fc44d85f4fd13450389a42dc2dc70b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-15 21:30:10 +0300

    feat : create experience page

---
+++ components/Experience.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+        <div className='py-20 ' id="projects">
+            <h1 className='heading'>
+                My {' '}
+                <span className='text-purple'>
+                    work experience
+                </span>
+            </h1>
+        </div>
+    )
+}
+
+export default Experience
