c5358a4e48bc7a9086b339db40c7bcb10569e234
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-17 21:02:02 +0300

    faet : footer mail button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Footer = () => {
     return (
@@ -16,9 +18,16 @@ const Footer = () => {
                 <h1 className='heading lg:max-w-[45vw]'>
                     Ready to take <span className='text-purple'>your</span> digital presence to the next level ?
                 </h1>
-                <p>
+                <p className='text-white-200 md:mt-10 my-5 text-center'>
                     Reach out me today and let&apos;s discuss how I can help you  achieve your goals.
                 </p>
+                <a href="mailto:ghostrex2@gmail.com">
+                    <MagicButton
+                        title="Let's get in touch"
+                        icon={<FaLocationArrow />}
+                        position='right'
+                    />
+                </a>
             </div>
         </footer>
     )
