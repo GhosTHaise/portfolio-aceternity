032f5601627efd3764de0d3b6a9ac1c270e9bbc6
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-06-17 21:12:05 +0300

    feat : show footer media button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
+import { socialMedia } from '@/data'
+import Image from 'next/image'
 
 const Footer = () => {
     return (
@@ -28,6 +30,28 @@ const Footer = () => {
                         position='right'
                     />
                 </a>
+                <div className='w-full flex mt-16 md:flex-row flex-col justify-between items-center'>
+                    <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 GhosT</p>
+
+                    <div className='flex items-center md:gap-3 gap-6'>
+                        {
+                            socialMedia.map((profile) => (
+                                <div key={profile.id} className='w-10 h-10 cursor-pointer 
+                                flex justify-center items-center backdrop-blur-lg saturate-150
+                                bg-opacity-75 bg-black-200 rounded-lg border border-black-300
+                                '>
+                                    <Image
+                                        src={profile.img}
+                                        alt={profile.id.toString()}
+                                        width={20}
+                                        height={20}
+                                        className=""
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
         </footer>
     )
